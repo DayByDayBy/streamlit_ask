c5ef87ffcde396fe7ba247ad8a26dbe8f3740446
Diomedes Git <diomedesgit@gmail.com>
2024-03-26 00:24:07 +0000

    tweak, tweak

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,38 @@ with st.sidebar:
     add_vertical_space(15)
     st.write('thrown together by [Бог](<https://www.boag.dev>)')
     
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ['Hi!']
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Hi!']
+        
+input_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+
+
+
+def get_text():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+with input_container:
+    user_input = get_text()
+    
+def generate_response(prompt):
+    chatbot = hugchat.ChatBot()
+    response = chatbot.chat(prompt)
+    return response
+
+
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+        
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['generated'][i], key=str(i))
